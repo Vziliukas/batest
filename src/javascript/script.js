4bbfcc639d4646e14e7a4ac50abc4eb99395fc40
Vytautas Ziliukas <>
2018-02-22 11:13:57 +0200

    Added click handler in form

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,1 +1,25 @@
-console.log('Hello');
+const btn = document.querySelector('button');
+const inputs = document.querySelectorAll('input');
+
+const data = {};
+
+console.log(inputs);
+
+btn.addEventListener('click', handleClick);
+inputs.forEach((input => input.addEventListener('change', handleChange)));
+
+function handleClick(e) {
+    //to prevent default action
+    e.preventDefault();
+    alert(`
+        Hello! ${data.name} ${data.lastName}
+        email: ${data.email}
+    `);
+}
+
+function handleChange() {
+    if(this.name == "name") data.name = this.value;
+    if(this.name == "lastName") data.lastName = this.value;
+    if(this.name == "email") data.email = this.value;
+}
+
