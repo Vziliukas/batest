672a2896145032a03a7624552b46aa3b74ef2385
Vytautas Ziliukas <>
2018-02-22 11:43:06 +0200

    Added simple validation to prevent button click

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,25 +1,31 @@
 const btn = document.querySelector('button');
 const inputs = document.querySelectorAll('input');
-
 const data = {};
-
-console.log(inputs);
 
 btn.addEventListener('click', handleClick);
 inputs.forEach((input => input.addEventListener('change', handleChange)));
+inputs.forEach((input => input.addEventListener('keyup', handleChange)));
 
 function handleClick(e) {
     //to prevent default action
     e.preventDefault();
-    alert(`
+     {
+        alert(`
         Hello! ${data.name} ${data.lastName}
         email: ${data.email}
     `);
+    }
 }
 
 function handleChange() {
+    const regexp = new RegExp('@', '\gi');
     if(this.name == "name") data.name = this.value;
     if(this.name == "lastName") data.lastName = this.value;
     if(this.name == "email") data.email = this.value;
+    if (data.name && data.lastName && regexp.test(data.email)){
+        btn.disabled = false;
+    } else {
+        btn.disabled = true;
+    }
 }
 
